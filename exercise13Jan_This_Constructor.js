1250049a7012527848337330298c376c51c9eee2
guitongli <guitong.lee.contact@gmail.com>
2021-01-13 22:29:59 +0100

    updated 13Jan

---
+++ exercise13Jan_This_Constructor.js
@@ -0,0 +1,44 @@
+//1. sorry it doesn't seem correctly answering the requirements but it works on my console...
+
+function Rectangle(width, height) {
+    this.width,
+        this.height,
+        (this.getArea = function (width, height) {
+            return this.width * this.height;
+        });
+}
+
+function Square(param3) {
+    (this.width = param3),
+        (this.height = param3),
+        (this.getArea = new Rectangle(param3, param3).getArea());
+}
+
+// 2.
+function invertCase(string) {
+    var chars = string.split("");
+
+    for (var i = 0; i < chars.length; i++) {
+        if (chars[i] == chars[i].toUpperCase()) {
+            chars[i] = chars[i].toLowerCase();
+        } else {
+            chars[i] = chars[i].toUpperCase();
+        }
+    }
+    return chars.join("");
+}
+
+// Bonus task: this doesn't run and I am stuck here.
+
+function Countdown(arg) {
+    this.initNum = arg;
+    this.start = function () {
+        function show() {
+            console.log(i);
+        }
+
+        for (var i = this.initNum; i > 0; i--) {
+            setTimeout(show, 1000);
+        }
+    };
+}
