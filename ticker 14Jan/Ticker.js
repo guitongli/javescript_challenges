4b3dbb352c90babb4a2fe762ef88ab7517ab0e6b
guitongli <guitong.lee.contact@gmail.com>
2021-01-15 08:37:37 +0100

    ticker is stuck

--- ticker 14Jan/Ticker.js
+++ ticker 14Jan/Ticker.js
@@ -1,12 +1,24 @@
-(function () {
-    var news = document.getElementById("wrapper");
+// was stuck and not working yet. will finish it today 
+
+
+(function animation () {
+    var news = document.getElementById("headlines");
 
     var toLeft = news.offsetLeft;
+    var firstChild = document.querySelector(a)
 
     function newsLine() {
         news.style.left--;
-        if (toleft < headlines.firstChild.offsetWidth) {
+        if (toleft <= headlines.firstChild.offsetWidth) {
             console.log(left);
-        requestAnimationFrame(news);
+        } else {
+            news.style.left += headlines.firstChild.offsetWidth;
+            headlines.appendChild(firstChild);
+            headlines.remove(firstChild)
+        }
+        news.style.left = news.offsetLeft
+        
+        requestAnimationFrame(newsLine;
     }
 });
+animation ()
