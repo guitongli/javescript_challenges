073c4c1ce78ff9cc85b6e2da6d6a2ee8261f1743
guitongli <guitong.lee.contact@gmail.com>
2021-01-15 08:15:13 +0100

    ticker is not finished

--- ticker 14Jan/Ticker.js
+++ ticker 14Jan/Ticker.js
@@ -1,3 +1,12 @@
 (function () {
-    var headline = document.getElementById("");
+    var news = document.getElementById("wrapper");
+
+    var toLeft = news.offsetLeft;
+
+    function newsLine() {
+        news.style.left--;
+        if (toleft < headlines.firstChild.offsetWidth) {
+            console.log(left);
+        requestAnimationFrame(news);
+    }
 });
