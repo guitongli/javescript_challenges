68292d5ed4bf16429fba9e7cb1e1d01b768426cb
guitongli <guitong.lee.contact@gmail.com>
2021-01-15 07:51:38 +0100

    updated exercises on 14 Jan

--- exercise14Jan_Dom.js
+++ exercise14Jan_Dom.js
@@ -2,7 +2,7 @@
 //The function should find all the elements in the document that match the selector and change their style so that the text they contain is italic, underlined, and bold.
 
 function changeStyle(str) {
-    var selectedElements = document.querySelector(str);
+    var selectedElements = document.querySelectorAll(str);
     for (var i = 0; i < selectedElements.length; i++) {
         selectedElements[i].style.fontStyle = "italic";
         selectedElements[i].style.textDecoration = "underline";
@@ -11,7 +11,7 @@ function changeStyle(str) {
 }
 //2. Write a function that expects a string representing a class name to be passed as a parameter. The function should return an array containing all the elements in the document that have the class that was passed in.
 function makeArr(str) {
-    var foundElements = document.querySelector(str);
+    var foundElements = document.querySelectorAll(str);
     for (var i = 0; i < foundElements.length; i++) {
         var showElements = [];
         showElements.push(foundElements[i]);
@@ -19,14 +19,13 @@ function makeArr(str) {
     return showElements;
 }
 //3. Write a function that inserts an element into the body of the currently loaded page. That element should have fixed position, z-index of 2147483647, left of 20px, top of 100px, font-size of 200px, and contain the text 'AWESOME'.
-function addElem() {
+funtion insertElem ()  {  
     var newElem = document.createElement("newElement");
     var newText = document.createTextNode("AWESOME");
     newElem.appendChild(newText);
-    document.body.append(newElem);
-    var elem = document.getElementsByTagName("newElement");
-    elem.style.position = "fixed";
-    elem.style.zIndex = "2147483647";
-    elem.style.left = "20px";
-    elem.style.fontSize = "200px";
+    document.body.appendChild(newElem);
+    newElem.style.position = "fixed";
+    newElem.style.zIndex = "2147483647";
+    newElem.style.left = "20px";
+    newElem.style.fontSize = "200px";
 }
---
+++ ticker 14Jan/Ticker.js
@@ -0,0 +1,3 @@
+(function () {
+    var headline = document.getElementById("");
+});
