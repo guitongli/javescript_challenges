e6bfe478c37de1a4e2fe634c0ebb86b9eb807924
guitongli <guitong.lee.contact@gmail.com>
2021-01-18 14:48:46 +0100

    need help on ticker

---
+++ canvas18Jan/canvas.js
@@ -0,0 +1,6 @@
+var smallCanvas = document.getElementById("bigCanvas");
+var ctx = smallCanvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.arc(100, 100, 30, 0, Math.PI * 2);
