d9c710a4594acdd6a0142ddf0306eafbfcc3b0e8
guitongli <guitong.lee.contact@gmail.com>
2021-01-18 18:39:37 +0100

    navbar works:D

--- Jan18_Canvas/canvas.js
+++ Jan18_Canvas/canvas.js
@@ -1,6 +1,42 @@
-var bigCanvas = document.getElementById("bigCanvas");
-var ctx = smallCanvas.getContext("2d");
+//bonus task pending
+
+var stickMan = document.getElementById("stickMan");
+
+var ctx = stickMan.getContext("2d");
+
+ctx.strokeStyle = "black";
+ctx.lineWidth = 6;
 
 ctx.beginPath();
-ctx.strokeStyle = "black";
 ctx.arc(100, 100, 30, 0, Math.PI * 2);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(100, 130);
+ctx.lineTo(100, 200);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(20, 110);
+ctx.lineTo(100, 150);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(180, 110);
+ctx.lineTo(100, 150);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(100, 200);
+ctx.lineTo(25, 280);
+ctx.stroke();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(100, 200);
+ctx.lineTo(175, 280);
+ctx.stroke();
+ctx.closePath();
---
+++ Jan18_hamburger/hamburger.js
@@ -0,0 +1,20 @@
+var hamburger = document.getElementById("menu");
+var navBar = document.querySelector(".nav-bar");
+var cross = document.querySelector(".x");
+var overLay = document.querySelector(".dark-chunk");
+
+hamburger.addEventListener("click", function () {
+    navBar.classList.add("on");
+    overLay.style.zIndex = 1000;
+});
+
+cross.addEventListener("click", function () {
+    navBar.classList.add("off");
+    overLay.style.zIndex = -1;
+});
+
+hamburger.addEventListener("click", function () {
+    navBar.classList.remove("off");
+    navBar.classList.add("on");
+    overLay.style.zIndex = 1000;
+});
