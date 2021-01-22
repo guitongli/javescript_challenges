1d5a9cb35b9345b3618c4720c6c90002ef8e240f
guitongli <guitong.lee.contact@gmail.com>
2021-01-22 08:59:44 +0100

    still trying things out

--- Jan21_Pane/pane.js
+++ Jan21_Pane/pane.js
@@ -1,13 +1,19 @@
 slider = $(".slider");
 top = $(".top");
 bottom = $(".bottom");
-
+console.log(slider);
 slider.mousedown(function () {
     var offset = $(this).offset();
     console.log(offset.left);
     slider.css("left", offset.left - 5);
     top.css("width", offset.left);
 });
+
+//trying things out...
+
+// $(function () {
+//     "slider".draggable();
+// });
 
 slider
     .mousemove(function () {
