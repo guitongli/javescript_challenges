09367a4c325aee4c884dcc0ca65421f99e870e46
guitongli <74822129+guitongli@users.noreply.github.com>
2021-02-11 08:50:34 +0100

    to be completed today due to stuck train back to berlin

--- Feb09_express_handlebars/index.js
+++ Feb09_express_handlebars/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const hb = require('express-handlebars')
-const projects = require('./data')
+const projects = require('./list')
 
 app.engine('handlebars', hb());
 app.set('view engine', 'handlebars')
@@ -10,17 +10,25 @@ app.use(express.static('./public'));
 app.use(express.static('./projects'));
 
 app.get('/', (req, res) =>{
-res.render('list', {
+  // res.writeHead(200);
+res.render('index', {
   layout: 'welcoming',
   projects
 })
+})
+app.get('/projects/:project',(req,res) =>{
+  // var profile = req.params.project
+  res.render('projects', {
+    layout: 'project',
+    projects
+  })
 })
 
 //takes to arguments, first is the template name, second is a config obj.
 // {{#array}} is shorthand for looping array {{#each array}}
 
-{/* <ul>
-  {{#teachers}}
+/* <ul>
+  {{#teachrs}}
       <li> this
-</li> */}
+</li> */
 app.listen(8080, () => console.log('hi'))
