99e77a6a3e467f25f509aac6237b0e38a483d78b
guitongli <guitong.lee.contact@gmail.com>
2021-02-11 22:57:32 +0100

    solved merge conflict

--- Feb08_express/index.js
+++ Feb08_express/index.js
@@ -3,16 +3,12 @@ const express = require('express');
 const app = express(); 
 const cookieParser = require('cookie-parser')
 const {projectList} = require('./listModule.js')
-<<<<<<< HEAD
 const basicAuth = require('basic-auth') 
-=======
->>>>>>> 09367a4c325aee4c884dcc0ca65421f99e870e46
 
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser());
 app.use(express.static('./projects'));
 // app.use(express.static(path.join(__dirname, 'static')));
-<<<<<<< HEAD
 app.use(
     (req, res, next) => {
 		  
@@ -40,13 +36,6 @@ app.use(
         res.sendStatus(401);
     }
 );
-=======
-app.use((req, res, next) =>{  
-	
-	next()
-});
-
->>>>>>> 09367a4c325aee4c884dcc0ca65421f99e870e46
 app.get('/', (req, res) =>{  
 
 	if (!req.cookies.authenticated){
@@ -55,40 +44,25 @@ app.get('/', (req, res) =>{
 	res.send(`<h1>Guitong's crappy Portfolio</h1> ${projectList()}`)
 }
 })
-<<<<<<< HEAD
  
 app.get('/cookie',(req, res)=>{
 	
-=======
-app.post('/', (req,res) => {
-	console.log(req.body)
-})
-app.get('/cookie',(req, res)=>{
->>>>>>> 09367a4c325aee4c884dcc0ca65421f99e870e46
 	res.send(`<form method='POST' style="display: flex; flex-direction: column; justify-content: space-between; width: 40%; height: 50%;">
 	<div>
                 <input type="checkbox" name="cookiesAgreement"><span>Would you like to receive our newsletter?</span>
             </div>
             <button> submit </submit>
         </form>`)
-<<<<<<< HEAD
 		console.log("im here")
-=======
->>>>>>> 09367a4c325aee4c884dcc0ca65421f99e870e46
 				
 			})
 
 app.post('/cookie',(req,res)=>{
 	if(req.body.cookiesAgreement == 'on'){
 		res.cookie('authenticated','true');
-<<<<<<< HEAD
 		res.redirect(req.cookies.url);
 		console.log(req.cookies.url);
 		res.clearCookie('url')
-=======
-		res.redirect('/');
-		console.log("ji")
->>>>>>> 09367a4c325aee4c884dcc0ca65421f99e870e46
 	} else {
 		res.send("no cookie no page")
 	}
--- Feb09_express_handlebars/index.js
+++ Feb09_express_handlebars/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const app = express();
 const hb = require("express-handlebars");
@@ -39,33 +38,6 @@ app.get("/projects/:project", (req, res) => {
         title: `highlight: ${current.item}`,
     });
 });
-=======
-const express = require('express');
-const app = express();
-const hb = require('express-handlebars')
-const projects = require('./list')
-
-app.engine('handlebars', hb());
-app.set('view engine', 'handlebars')
-
-app.use(express.static('./public'));
-app.use(express.static('./projects'));
-
-app.get('/', (req, res) =>{
-  // res.writeHead(200);
-res.render('index', {
-  layout: 'welcoming',
-  projects
-})
-})
-app.get('/projects/:project',(req,res) =>{
-  // var profile = req.params.project
-  res.render('projects', {
-    layout: 'project',
-    projects
-  })
-})
->>>>>>> 09367a4c325aee4c884dcc0ca65421f99e870e46
 
 //takes to arguments, first is the template name, second is a config obj.
 // {{#array}} is shorthand for looping array {{#each array}}
@@ -74,8 +46,4 @@ app.get('/projects/:project',(req,res) =>{
   {{#teachrs}}
       <li> this
 </li> */
-<<<<<<< HEAD
 app.listen(8080, () => console.log("hi"));
-=======
-app.listen(8080, () => console.log('hi'))
->>>>>>> 09367a4c325aee4c884dcc0ca65421f99e870e46
