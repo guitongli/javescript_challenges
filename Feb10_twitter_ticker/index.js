2fda5cf11efb0bbd0bd3c9779ee4dfa587d3b522
guitongli <guitong.lee.contact@gmail.com>
2021-02-12 08:58:07 +0100

    getToken successful, others pending

--- Feb10_twitter_ticker/index.js
+++ Feb10_twitter_ticker/index.js
@@ -1,28 +1,26 @@
-const express =require('express');
-const app = express ();
-const {getToken, getTweets, filterTweets} = require("./twitter")
+const express = require("express");
+const app = express();
+const { getToken, getTweets, filterTweets } = require("./twitter");
 
 app.use(express.static("./ticker"));
 // need to figure out how this works
 
+function bearerToken(t) {
+    console.log(t);
+}
 
-app.get('/data.json', (req,res) =>{
-    console.log("requiring")
-    getToken((err, bearerToken) =>{
-        if(err){
-            console.log(err)
-            res.sendStatus(500)
-        } else {
-            console.log(bearerToken)
-            getTweets(bearerToken, (err,tweets)=>{
-                 const filteredTweets = filterTweets(tweets);
-    res.json(filterTweets)
-            });
-   
-        }
-        
-    });
-    
-})
+app.get("/", (req, res) => {
+    console.log("requiring");
+    getToken();
+    // getToken((err, bearerToken) => {
+    //     if (err) {
+    //         console.log(err);
+    //         res.sendStatus(500);
+    //     } else {
+    //         console.log(bearerToken);
+    // getTweets(bearerToken, (err, tweets) => {
+    //     const filteredTweets = filterTweets(tweets);
+    //     res.json(filterTweets);
+});
 
-app.listen(8080, ()=> console.log("hi"))
+app.listen(8080, () => console.log("hi"));
--- Feb10_twitter_ticker/twitter.js
+++ Feb10_twitter_ticker/twitter.js
@@ -1,54 +1,71 @@
 // developer.twitter.com/
 // tuthentication
 //review ajax
-const secret = require('.secrets');
-const {twitterKey, twitterSecret} = secret;
+const secrets = require("./secrets");
+const { key, secret } = secrets;
 
 // need to figure out how prettier works
 
-const https = require("https")
+const https = require("https");
 // to talk to the api
+var bearerToken = "";
+module.exports.getToken = function getToken() {
+    // bearerToken
+    let credentials = `${key}:${secret}`;
+    let encodedCredentials = Buffer.from(credentials).toString("base64");
 
-module.exports.getTocken = function getTocken(callbackToken){
-// bearerToken
-let credentials = `${twitterKey}:${twitterSecret}`;
-let encodedCredentials = Buffer.from(credentials).toString('base64');
-const options ={
-    host: "api.twitter,com",
-    path:"/oauth2/token",
-    method:"POST",
-    headers: {
-        Authorization: `Basic ${encodedCredentials}`,
-        "Content-Type":"application/x-www-form-urlencoded;charset=UTF-8"
-    }
-}
-function reqCallback(response){
-    if(response.statusCode !=200){
-        callbackTocken(response.statusCode);
-        
-        return;
+    const options = {
+        host: "api.twitter.com",
+        path: "/oauth2/token",
+        method: "POST",
 
-    }
-    let body='';
-    response.on('data', (chunk)=>{
-        body += chunk;
+        headers: {
+            Authorization: `Basic ${encodedCredentials}`,
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        },
+    };
+    // function reqCallback(response){
+    //     if(response.statusCode !=200){
+    //         callbackTocken(response.statusCode);
+
+    //         return;
+
+    //     }
+    //     let body='';
+    //     response.on('data', (chunk)=>{
+    //         body += chunk;
+    //     });
+
+    //     response.on('end', ()=> {
+    //         console.log("body" body);
+    //         let parsedBody = JSON.parse(body);
+    //         console.log("parsed", parsedBody.access_token);
+    //         callbackToken(null, parsedBody.access_token);
+    //         //null means actively setting everything is ok, whereas undefined is  the default and means nothing.
+    //     })
+    // }
+
+    const req = https.request(options, (res) => {
+        if (res.statusCode != "200") {
+            console.log(res.statusCode);
+            return;
+        }
+        let body = "";
+        res.on("data", (chunk) => {
+            body += chunk;
+        });
+        req.on("error", (error) => {
+            console.error(error);
+        });
+        res.on("end", () => {
+            bearerToken = JSON.parse(body).access_token;
+        });
     });
-
-    response.on('end', ()=> {
-        console.log("body" body);
-        let parsedBody = JSON.parse(body);
-        console.log("parsed", parsedBody.access_token);
-        callbackToken(null, parsedBody.access_token);
-        //null means actively setting everything is ok, whereas undefined is  the default and means nothing.
-    })
-}
-const req = https.request(options, reqCallback);
-req.end()
-}
-module.exports.getTweets = function getTweets(bearerTocken, callbackTweets){
-    //
+    req.end("grant_type=client_credentials");
 };
+console.log(bearerToken);
+// module.exports.getTweets = function getTweets(bearerTocken, callbackTweets) {};
 
-module.exports.filterTweets =function filterTweets(tweets){
-    // clean up / filter our tween response.
-}
+// module.exports.filterTweets = function filterTweets(tweets) {
+//     // clean up / filter our tween response.
+// };
